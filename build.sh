e0878d72fab6b39a2fb3ee01facc4df242484a62
Michael Mendy <montana@linux.com>
2023-12-07 16:09:20 -0800

    Push/update

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-echo "$ROBOT_PASSWORD" | docker login -u "$ROBOT_USERNAME" --password-stdin quay.io
-docker push quay.io/montana/s390x2
+
+ARCH=$(uname -m)
+
+LD_DEBUG=libs docker build -t container/$ARCH -f ./$ARCH/Dockerfile .
+docker tag container/$ARCH quay.io/montana/multi-arch:multi-arch-travis-$ARCH
+docker login quay.io -u "$ROBOT_USER" -p $ROBOT_TOKEN
+docker push quay.io/montana/multi-arch:multi-arch-travis-$ARCH
